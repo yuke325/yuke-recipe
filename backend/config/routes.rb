fcb99100c235411a6de663e116547b65ce26d420
yuke <yusukesaikyou931@gmail.com>
2026-03-09 17:21:21 +0900

    feat: add recipe controller

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get "recipes/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "/hello", to: "hello#index"
+  get "/recipes", to: "recipes#index"
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
