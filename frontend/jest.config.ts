1696d574ef167428c5774884ab3e86fbe9f9b0f2
yuke <yusukesaikyou931@gmail.com>
2026-03-11 06:14:53 +0900

    build: install jest, testing library

---
+++ frontend/jest.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from "jest";
+import nextJest from "next/jest.js";
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const config: Config = {
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+};
+
+export default createJestConfig(config);
