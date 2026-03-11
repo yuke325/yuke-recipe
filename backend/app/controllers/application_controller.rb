016b418f24df1cd68b752881bb421b572b38cbc4
yuke <yusukesaikyou931@gmail.com>
2026-03-11 17:34:23 +0900

    feat: エラーハンドリング（404, 422)

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+
+  private
+
+  def render_not_found(_exception)
+    render json: {
+      errors: [
+        { code: 'not_found', message: 'Resource not found' }
+      ]
+    }, status: :not_found
+  end
+
+  def render_unprocessable_entity(exception)
+    render json: {
+      errors: exception.record.errors.full_messages.map do |message|
+        { code: 'unprocessable_entity', message: message }
+      end
+    }, status: :unprocessable_entity
+  end
 end
