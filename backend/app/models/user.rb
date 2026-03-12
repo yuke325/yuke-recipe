9c622214d99f9696b7e7cafac159e6f7e67eddf4
yuke <yusukesaikyou931@gmail.com>
2026-03-12 16:04:02 +0900

    feat: nameとmailは255文字以下のバリデーション追加

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :recipes, dependent: :destroy
 
-  validates :name, presence: true
-  validates :email, presence: true, uniqueness: true
+  validates :name, presence: true, length: { maximum: 255 }
+  validates :email, presence: true, uniqueness: true, length: { maximum: 255 }
   validates :password_hash, presence: true
 end
