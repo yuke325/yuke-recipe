000eedaa2e703c5099380816bc0fa136978e9129
yuke <yusukesaikyou931@gmail.com>
2026-03-16 16:53:45 +0900

    feat: user_idを固定してフロント側が常にレシピの操作可能

--- backend/app/controllers/recipes_controller.rb
+++ backend/app/controllers/recipes_controller.rb
@@ -1,4 +1,6 @@
 class RecipesController < ApplicationController
+  DEFAULT_RECIPE_USER_EMAIL = 'recipe-dev@example.com'.freeze
+
   def index
     recipes = Recipe.all
     render json: RecipeResource.new(recipes).serialize
@@ -10,7 +12,7 @@ class RecipesController < ApplicationController
   end
 
   def create
-    recipe = Recipe.create!(recipe_params)
+    recipe = Recipe.create!(recipe_params.merge(user: default_recipe_user))
     render json: RecipeResource.new(recipe).serialize, status: :created
   end
 
@@ -31,6 +33,17 @@ class RecipesController < ApplicationController
   private
 
   def recipe_params
-    params.require(:recipe).permit(:user_id, :name, :description)
+    recipe_attributes.permit(:name, :description)
+  end
+
+  def recipe_attributes
+    params.key?(:recipe) ? params.require(:recipe) : params
+  end
+
+  def default_recipe_user
+    User.find_or_create_by!(email: DEFAULT_RECIPE_USER_EMAIL) do |user|
+      user.name = 'Recipe Demo User'
+      user.password_hash = 'temporary-password-hash'
+    end
   end
 end
