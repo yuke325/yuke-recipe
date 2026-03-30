05d4d0356a6d700f8497b554fb8b59d7c3cbe465
yuke <yusukesaikyou931@gmail.com>
2026-03-30 17:29:57 +0900

    backend: scope recipes to current user

--- backend/app/controllers/recipes_controller.rb
+++ backend/app/controllers/recipes_controller.rb
@@ -1,30 +1,30 @@
 class RecipesController < ApplicationController
-  DEFAULT_RECIPE_USER_EMAIL = 'recipe-dev@example.com'.freeze
+  before_action :authenticate_user!
 
   def index
-    recipes = Recipe.all
+    recipes = current_user.recipes
     render json: RecipeResource.new(recipes).serialize
   end
 
   def show
-    recipe = Recipe.find(params[:id])
+    recipe = current_user.recipes.find(params[:id])
     render json: RecipeResource.new(recipe).serialize
   end
 
   def create
-    recipe = Recipe.create!(recipe_params.merge(user: default_recipe_user))
+    recipe = current_user.recipes.create!(recipe_params)
     render json: RecipeResource.new(recipe).serialize, status: :created
   end
 
   def update
-    recipe = Recipe.find(params[:id])
+    recipe = current_user.recipes.find(params[:id])
     recipe.update!(recipe_params)
 
     render json: RecipeResource.new(recipe).serialize
   end
 
   def destroy
-    recipe = Recipe.find(params[:id])
+    recipe = current_user.recipes.find(params[:id])
     recipe.destroy!
 
     head :no_content
@@ -38,12 +38,5 @@ class RecipesController < ApplicationController
 
   def recipe_attributes
     params.key?(:recipe) ? params.require(:recipe) : params
-  end
-
-  def default_recipe_user
-    User.find_or_create_by!(email: DEFAULT_RECIPE_USER_EMAIL) do |user|
-      user.name = 'Recipe Demo User'
-      user.password_hash = 'temporary-password-hash'
-    end
   end
 end
