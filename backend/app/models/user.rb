9386c795e215d1b7730726819225b6901dca59ff
yuke <yusukesaikyou931@gmail.com>
2026-03-30 17:29:41 +0900

    backend: add google subject to users

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :recipes, dependent: :destroy
 
+  validates :google_sub, presence: true, uniqueness: true, length: { maximum: 255 }
   validates :name, presence: true, length: { maximum: 255 }
   validates :email, presence: true, uniqueness: true, length: { maximum: 255 }
-  validates :password_hash, presence: true
 end
